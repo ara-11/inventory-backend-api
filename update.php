9aecdcc78e2f924d548c1f739cf1c1741e5433b2
ara duco <araduco3@gmail.com>
2025-07-05 00:32:18 +0800

    removed unnecessary comments

--- update.php
+++ update.php
@@ -5,20 +5,7 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Content-Type: application/json");
 header("X-Content-Type-Options: nosniff");
-/*
-include 'db.php';
 
-$data = json_decode(file_get_contents("php://input"));
-
-if(isset($data->id) && isset($data->name) && isset($data->quantity) && isset($data->price)) {
-  $stmt = $conn->prepare("UPDATE products SET name=?, quantity=?, price=? WHERE id=?");
-  $stmt->execute([$data->name, $data->quantity, $data->price, $data->id]);
-
-  echo json_encode(["message" => "Product updated successfully"]);
-} else {
-  echo json_encode(["message" => "Invalid data"]);
-}
- */
 include 'db.php';
 
 $data = json_decode(file_get_contents("php://input"));
